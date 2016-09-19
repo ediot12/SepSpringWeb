3b9f4c1ad061e20dac908598bac74f3c6cb2b961
user1 <user1@user1-PC>
2016-09-19 15:00:28 +0900

    ds

---
+++ SepSpringWeb/src/main/java/spring/controller/MemberInfoValidator.java
@@ -0,0 +1,31 @@
+package spring.controller;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class MemberInfoValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		// TODO Auto-generated method stub
+		return MemberInfo.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		// TODO Auto-generated method stub
+
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userId", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "address.address1", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "address.address2", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "jobCode", "required");
+		MemberInfo memberInfo = (MemberInfo) target;
+		if (memberInfo.getFavorites() == null || memberInfo.getFavorites().length == 0) {
+			errors.rejectValue("favorites", "must_select");
+		}
+
+	}
+
+}
